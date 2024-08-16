840724f7c60885c313280ec3f28956d4717f1564
roman <51534566+roman-thapa@users.noreply.github.com>
2024-08-16 10:09:41 +0530

    Fetch images from Picsum API and display them on the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [score, setScore] = useState(0)
-  const [data, setData] = useState()
+  const [score, setScore] = useState(0);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
-      const data = response.json()
-      setData(data)
-    }
-    fetchData()
-  }, [])
-  console.log(data)
+      try {
+        const response = await fetch(
+          "https://picsum.photos/v2/list?page=2&limit=10"
+        );
+        const result = await response.json();
+        setData(result);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  if (error) return <div>Error: {error.message}</div>;
   return (
     <>
       <h1>Memory card Game</h1>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : data ? (
+        data.map((elm) => (
+          <img
+            key={elm.id}
+            src={elm.download_url}
+            alt={elm.author}
+            className="card"
+            style={{ width: "15rem", height: "auto" }}
+          />
+        ))
+      ) : (
+        <h3>No data Found</h3>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
